9cc1cab30feaeaa3d57ffa5121e3e2a95158ae0d
franciscoblancojn <blancofrancisco34@gmail.com>
2025-08-22 10:49:15 -0500

    flb

--- cjs/src2/api/_.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.API_URL_TRANSPORT = exports.API_URL_UPDATE_GUIA = exports.API_URL_QUOTE = exports.API_URL_CITY = exports.API_URL_AGENTE = exports.API_URL_AUTHENTICATE = void 0;
-exports.API_URL_AUTHENTICATE = 'https://app.aveonline.co/api/comunes/v1.0/autenticarusuario.php';
-exports.API_URL_AGENTE = 'https://app.aveonline.co/api/comunes/v1.0/agentes.php';
-exports.API_URL_CITY = 'https://app.aveonline.co/api/box/v1.0/ciudad.php';
-exports.API_URL_QUOTE = 'https://app.aveonline.co/api/nal/v1.0/generarGuiaTransporteNacional.php';
-exports.API_URL_UPDATE_GUIA = 'https://app.aveonline.co/api/nal/v1.0/plugins/wordpress.php';
-exports.API_URL_TRANSPORT = 'https://app.aveonline.co/api/box/v1.0/transportadora.php';
