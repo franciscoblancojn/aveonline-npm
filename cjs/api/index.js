2632e78766cfd8f70cace76c24bc159a753c51cf
franciscoblancojn <blancofrancisco34@gmail.com>
2025-09-10 09:13:02 -0500

    flb

--- cjs/api/index.js
+++ cjs/api/index.js
@@ -37,7 +37,7 @@ class AveApi {
         this.user = auth;
         this.user.idempresa = auth?.cuentas?.[0]?.usuarios?.[0]?.id;
         let dataAuth = {
-            token: auth.token,
+            token: auth?.token,
             idempresa: auth?.cuentas?.[0]?.usuarios?.[0]?.id,
         };
         this.agents = new agents_1.AveApiAgents(dataAuth);
@@ -50,7 +50,7 @@ class AveApi {
             const auth2 = await this.auth.onAuthenticate2(this.data);
             this.user2 = auth2;
             dataAuth = {
-                token: auth2.data.token,
+                token: auth2?.data?.token,
                 idempresa: auth2?.data?.idEnterprise,
             };
         }
